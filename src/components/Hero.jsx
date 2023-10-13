a414ba54d3bb6fc5f27f15f9197b776e393ae1c6
noeamelya <noeamelyaofficial@hotmail.com>
2023-10-13 23:49:28 +0100

    update file

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,7 @@ import imglogo from "../assets/Logo.png"
 
 const Hero = () => {
   return (
-    <header className='w-full flex justify-center items-center flex-col'>
+    <header className='w-full flex justify-center items-center flex-col md:mb-4'>
         <div className="flex justify-center items-center">
             <img src={imglogo} alt="logo" className='w-40 object-contain my-10' />
         </div>
@@ -11,7 +11,7 @@ const Hero = () => {
             We're sorry, <br className='max-md:hidden'/>
             <span className='orange_gradient'>our website is down..</span>
         </h1>
-        <h2 className="text-slate-600 mb-10 justify-ceenter items-center md:mb-4 "> We apolozise that maintenance is taking longer than expected.<br/>
+        <h2 className="text-slate-600 mb-10 "> We apolozise that maintenance is taking longer than expected.<br/>
         </h2>
         <h1 className="text-slate-600 mb-10">Thank you for your patience </h1>
 
